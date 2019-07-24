e31ce005f0b56ca672e8e60a42f0d5bee176e0e6
Reese Lee <reese@>
2019-07-24 16:25:39 -0700

    add component for Lists, plus title styling

---
+++ ClientApp/src/components/Lists.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+import './Home.css';
+import { Layout } from './Layout';
+
+export class Lists extends Component {
+  static displayName = Lists.name;
+
+  render() {
+    return (
+      <Layout>
+      <div>
+        <h1 className="title3">Lists</h1>
+      </div>
+      </Layout>
+    )
+  }
+}
