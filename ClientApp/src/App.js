ef568270901f8031e3c068ec2ae4f55b22f2e1a6
Reese Lee <reese@>
2019-07-24 16:22:14 -0700

    remove Layout, add routes for new components

--- ClientApp/src/App.js
+++ ClientApp/src/App.js
@@ -1,20 +1,28 @@
 import React, { Component } from 'react';
 import { Route } from 'react-router';
-import { Layout } from './components/Layout';
 import { Home } from './components/Home';
 import { FetchData } from './components/FetchData';
 import { Counter } from './components/Counter';
+import { Info } from './components/Info';
+import { Maps } from './components/Maps';
+import { Lists } from './components/Lists';
 
 export default class App extends Component {
   static displayName = App.name;
 
   render () {
     return (
-      <Layout>
-        <Route exact path='/' component={Home} />
+      <div>
+      <Route exact path='/' component={Home} />
+      <div>
         <Route path='/counter' component={Counter} />
         <Route path='/fetch-data' component={FetchData} />
-      </Layout>
+        <Route path='/info' component={Info} />
+        <Route path='/maps' component={Maps} />
+        <Route path='/lists' component={Lists} />
+
+      </div>
+      </div>
     );
   }
 }
