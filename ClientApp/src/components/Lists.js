8b16b379eb8c4d4b188f07d4af3747670445eadb
Reese Lee <reese@>
2019-07-24 16:46:47 -0700

    remove a rogue letter

--- ClientApp/src/components/Lists.js
+++ ClientApp/src/components/Lists.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import './Home.css';
-import { Layout } from './Layout';Í 
+import { Layout } from './Layout';
 
 export class Lists extends Component {
   static displayName = Lists.name;
@@ -10,6 +10,11 @@ export class Lists extends Component {
       <Layout>
       <div>
         <h1 className="title3">Lists</h1>
+          <div className="buttonDiv">
+            <button className="splashButtons">Bugout Bag</button><br/>
+            <button className="splashButtons">Food</button><br/>
+            <button className="splashButtons">other supplies</button>
+          </div>
       </div>
       </Layout>
     )
