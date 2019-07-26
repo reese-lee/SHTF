43299c567842bc356de3e68ebc63ed5271e9bb1b
Reese Lee <reese.lee.cy@gmail.com>
2019-07-26 15:31:59 -0700

    update controller to show Categories

--- Controllers/SampleDataController.cs
+++ Controllers/SampleDataController.cs
@@ -2,43 +2,21 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
-using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc; 
+using SHTF.Context;
 
-namespace SHTF01.Controllers
+namespace SHTF.Controllers
 {
     [Route("api/[controller]")]
-    public class SampleDataController : Controller
+    public class ListsController : Controller
     {
-        private static string[] Summaries = new[]
-        {
-            "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
-        };
 
         [HttpGet("[action]")]
-        public IEnumerable<WeatherForecast> WeatherForecasts()
+        public List<Category> GetCategories(int startDateIndex)
         {
-            var rng = new Random();
-            return Enumerable.Range(1, 5).Select(index => new WeatherForecast
-            {
-                DateFormatted = DateTime.Now.AddDays(index).ToString("d"),
-                TemperatureC = rng.Next(-20, 55),
-                Summary = Summaries[rng.Next(Summaries.Length)]
-            });
+            List<Category> category = SHTFListData.GetCategory();
+            return category;
         }
 
-        public class WeatherForecast
-        {
-            public string DateFormatted { get; set; }
-            public int TemperatureC { get; set; }
-            public string Summary { get; set; }
-
-            public int TemperatureF
-            {
-                get
-                {
-                    return 32 + (int)(TemperatureC / 0.5556);
-                }
-            }
-        }
     }
 }
