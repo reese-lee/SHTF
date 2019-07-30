3a680f093cd21ee7049d05cadd0ea4d2e1cdd9d4
Reese Lee <reese@>
2019-07-29 17:12:39 -0700

    add buttons back with links to new components

--- ClientApp/src/components/Info.jsx
+++ ClientApp/src/components/Info.jsx
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import './Home.css';
 import { Layout } from './Layout';
 import { UncontrolledCollapse, Button, CardBody, Card } from 'reactstrap';
+import Collapse from '@kunukn/react-collapse';
 
 export class Info extends Component {
   static displayName = Info.name;
@@ -23,39 +24,12 @@ export class Info extends Component {
           <h1 className="title3">INFO</h1>
 
           <div className="buttonDiv">
-            <div>
-              <Button className="splashButtons" id="toggler" style={{ marginBottom: '1rem' }}>The Big One</Button>
-              <UncontrolledCollapse toggler="#toggler">
-                <Card>
-                  <CardBody>
-                    Anim pariatur cliche reprehenderit,
-                    enim eiusmod high life accusamus terry richardson ad squid. Nihil
-                    anim keffiyeh helvetica, craft beer labore wes anderson cred
-                    nesciunt sapiente ea proident.
-                  </CardBody>
-                </Card>
-              </UncontrolledCollapse>
-            </div>
+            <button className="splashButtons" to='/infoOne'>One info</button><br/>
+            <button className="splashButtons">Two infos</button><br/>
+            <button className="splashButtons">The third info</button><br/>
+            <button className="splashButtons">And another one</button><br/>
           </div>
 
-          <div className="buttonDiv">
-            <div>
-              <Button className="splashButtons" id="toggler" style={{ marginBottom: '1rem' }}>The Other One</Button>
-              <UncontrolledCollapse toggler="#toggler">
-                <Card>
-                  <CardBody>
-                    Anim pariatur cliche reprehenderit,
-                    enim eiusmod high life accusamus terry richardson ad squid. Nihil
-                    anim keffiyeh helvetica, craft beer labore wes anderson cred
-                    nesciunt sapiente ea proident.
-                  </CardBody>
-                </Card>
-              </UncontrolledCollapse>
-            </div>
-
-
-          </div>
-          <br/>
 
         </div>
       </Layout>
