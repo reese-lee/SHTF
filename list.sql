71c24278defcc916dfe88d64f0a3dcf7677e5655
Reese Lee <reese.lee.cy@gmail.com>
2019-07-30 13:15:48 -0700

    add mysql database

---
+++ list.sql
@@ -0,0 +1,136 @@
+-- phpMyAdmin SQL Dump
+-- version 4.8.4
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1:3306
+-- Generation Time: Jul 30, 2019 at 08:13 PM
+-- Server version: 5.7.24
+-- PHP Version: 7.2.14
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `list`
+--
+CREATE DATABASE IF NOT EXISTS `list` DEFAULT CHARACTER SET latin1 COLLATE latin1_swedish_ci;
+USE `list`;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `category`
+--
+
+DROP TABLE IF EXISTS `category`;
+CREATE TABLE IF NOT EXISTS `category` (
+  `Id` int(11) NOT NULL AUTO_INCREMENT,
+  `Name` text,
+  PRIMARY KEY (`Id`)
+) ENGINE=MyISAM AUTO_INCREMENT=10 DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `category`
+--
+
+INSERT INTO `category` (`Id`, `Name`) VALUES
+(1, 'Bug Out Bag'),
+(3, 'Contact'),
+(4, 'Food'),
+(5, 'Supply');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `contact`
+--
+
+DROP TABLE IF EXISTS `contact`;
+CREATE TABLE IF NOT EXISTS `contact` (
+  `ContactId` int(11) NOT NULL AUTO_INCREMENT,
+  `ContactName` text,
+  `ContactNote` text,
+  `ContactCatId` int(11) NOT NULL,
+  PRIMARY KEY (`ContactId`)
+) ENGINE=MyISAM DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `food`
+--
+
+DROP TABLE IF EXISTS `food`;
+CREATE TABLE IF NOT EXISTS `food` (
+  `FoodId` int(11) NOT NULL AUTO_INCREMENT,
+  `FoodName` text,
+  `FoodAmount` int(11) NOT NULL,
+  `FoodDone` bit(1) NOT NULL,
+  `FoodCatId` int(11) NOT NULL,
+  PRIMARY KEY (`FoodId`)
+) ENGINE=MyISAM DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `item`
+--
+
+DROP TABLE IF EXISTS `item`;
+CREATE TABLE IF NOT EXISTS `item` (
+  `ItemId` int(11) NOT NULL AUTO_INCREMENT,
+  `ItemName` text,
+  `ItemAmount` int(11) NOT NULL,
+  `ItemDone` bit(1) NOT NULL,
+  `ItemCatId` int(11) NOT NULL,
+  PRIMARY KEY (`ItemId`)
+) ENGINE=MyISAM DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `supply`
+--
+
+DROP TABLE IF EXISTS `supply`;
+CREATE TABLE IF NOT EXISTS `supply` (
+  `SupplyId` int(11) NOT NULL AUTO_INCREMENT,
+  `SupplyName` text,
+  `SupplyAmount` int(11) NOT NULL,
+  `SupplyDone` bit(1) NOT NULL,
+  `SupplyCatId` int(11) NOT NULL,
+  PRIMARY KEY (`SupplyId`)
+) ENGINE=MyISAM DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `__efmigrationshistory`
+--
+
+DROP TABLE IF EXISTS `__efmigrationshistory`;
+CREATE TABLE IF NOT EXISTS `__efmigrationshistory` (
+  `MigrationId` varchar(150) NOT NULL,
+  `ProductVersion` varchar(32) NOT NULL,
+  PRIMARY KEY (`MigrationId`)
+) ENGINE=MyISAM DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `__efmigrationshistory`
+--
+
+INSERT INTO `__efmigrationshistory` (`MigrationId`, `ProductVersion`) VALUES
+('20190726221358_InitialCreate', '2.2.6-servicing-10079');
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
