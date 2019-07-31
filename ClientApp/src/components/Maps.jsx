68c1c983ac6c78c08b5f607cad7dbf416ce27145
Reese Lee <reese@>
2019-07-31 15:38:59 -0700

    add iframe tag to embed google map showing Portland, plus styling

--- ClientApp/src/components/Maps.jsx
+++ ClientApp/src/components/Maps.jsx
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import './Home.css';
 import { Layout } from './Layout';
+import { Link } from 'react-router-dom';
 
 export class Maps extends Component {
   static displayName = Maps.name;
@@ -11,11 +12,10 @@ export class Maps extends Component {
       <div>
         <h1 className="title3">Maps</h1>
           <div>
-            MAPS WILL GO HERE
+            <iframe src="https://maps.google.com/maps?q=portland%20or&t=&z=13&ie=UTF8&iwloc=&output=embed" style={{width:"100%", height:"400px", frameborder:"none", border:0}} allowFullScreen></iframe>
           </div>
           <div className="buttonDiv">
-            <button className="splashButtons">Danger</button><br/>
-            <button className="splashButtons">Safe</button>
+            <button className="splashButtons"><Link to="/DangerZone">Danger Zones</Link></button><br/>
           </div>
       </div>
       </Layout>
