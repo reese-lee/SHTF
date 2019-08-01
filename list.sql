4fd16aac5b1d85e0889f41c6db70af641ce6a313
Reese Lee <reese.lee.cy@gmail.com>
2019-08-01 01:01:58 -0700

    add updated database with hardcoded items

--- list.sql
+++ list.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1:3306
--- Generation Time: Jul 30, 2019 at 08:13 PM
+-- Generation Time: Aug 01, 2019 at 07:53 AM
 -- Server version: 5.7.24
 -- PHP Version: 7.2.14
 
@@ -42,7 +42,7 @@ CREATE TABLE IF NOT EXISTS `category` (
 --
 
 INSERT INTO `category` (`Id`, `Name`) VALUES
-(1, 'Bug Out Bag'),
+(1, 'Item'),
 (3, 'Contact'),
 (4, 'Food'),
 (5, 'Supply');
@@ -60,7 +60,16 @@ CREATE TABLE IF NOT EXISTS `contact` (
   `ContactNote` text,
   `ContactCatId` int(11) NOT NULL,
   PRIMARY KEY (`ContactId`)
-) ENGINE=MyISAM DEFAULT CHARSET=latin1;
+) ENGINE=MyISAM AUTO_INCREMENT=4 DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `contact`
+--
+
+INSERT INTO `contact` (`ContactId`, `ContactName`, `ContactNote`, `ContactCatId`) VALUES
+(1, 'Police', '911', 3),
+(2, 'Non-emergency', '(503) 823-3333', 3),
+(3, 'Portland Bureau of Emergency Management', '(503) 823-4375', 3);
 
 -- --------------------------------------------------------
 
@@ -72,11 +81,25 @@ DROP TABLE IF EXISTS `food`;
 CREATE TABLE IF NOT EXISTS `food` (
   `FoodId` int(11) NOT NULL AUTO_INCREMENT,
   `FoodName` text,
-  `FoodAmount` int(11) NOT NULL,
-  `FoodDone` bit(1) NOT NULL,
   `FoodCatId` int(11) NOT NULL,
   PRIMARY KEY (`FoodId`)
-) ENGINE=MyISAM DEFAULT CHARSET=latin1;
+) ENGINE=MyISAM AUTO_INCREMENT=11 DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `food`
+--
+
+INSERT INTO `food` (`FoodId`, `FoodName`, `FoodCatId`) VALUES
+(1, 'Protein bars', 4),
+(2, 'MREs', 4),
+(3, 'Canned meat', 4),
+(4, 'Peanut butter', 4),
+(5, 'Canned soup', 4),
+(6, 'Dried fruit', 4),
+(7, 'Rice, grain, oats, etc.', 4),
+(8, 'Milk powder', 4),
+(9, 'Dried beans, peas, lentils, etc.', 4),
+(10, 'Shortening oils', 4);
 
 -- --------------------------------------------------------
 
@@ -88,11 +111,27 @@ DROP TABLE IF EXISTS `item`;
 CREATE TABLE IF NOT EXISTS `item` (
   `ItemId` int(11) NOT NULL AUTO_INCREMENT,
   `ItemName` text,
-  `ItemAmount` int(11) NOT NULL,
-  `ItemDone` bit(1) NOT NULL,
   `ItemCatId` int(11) NOT NULL,
   PRIMARY KEY (`ItemId`)
-) ENGINE=MyISAM DEFAULT CHARSET=latin1;
+) ENGINE=MyISAM AUTO_INCREMENT=13 DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `item`
+--
+
+INSERT INTO `item` (`ItemId`, `ItemName`, `ItemCatId`) VALUES
+(1, 'Bandages/Band-aids', 1),
+(2, 'Headlamp', 1),
+(3, 'Multi-tool', 1),
+(4, 'Waterproof matches', 1),
+(5, 'Water filtration system', 1),
+(6, 'Solar charger', 1),
+(7, 'Paracord', 1),
+(8, 'Thermal blanket', 1),
+(9, 'Anti-bacterial wipes', 1),
+(10, 'Tarp', 1),
+(11, 'Water bottle/bladder', 1),
+(12, 'Protein bars', 1);
 
 -- --------------------------------------------------------
 
@@ -104,11 +143,27 @@ DROP TABLE IF EXISTS `supply`;
 CREATE TABLE IF NOT EXISTS `supply` (
   `SupplyId` int(11) NOT NULL AUTO_INCREMENT,
   `SupplyName` text,
-  `SupplyAmount` int(11) NOT NULL,
-  `SupplyDone` bit(1) NOT NULL,
   `SupplyCatId` int(11) NOT NULL,
   PRIMARY KEY (`SupplyId`)
-) ENGINE=MyISAM DEFAULT CHARSET=latin1;
+) ENGINE=MyISAM AUTO_INCREMENT=13 DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `supply`
+--
+
+INSERT INTO `supply` (`SupplyId`, `SupplyName`, `SupplyCatId`) VALUES
+(1, 'Mini shovel', 5),
+(2, 'Axe', 5),
+(3, 'Duct tape', 5),
+(4, 'Pepper spray', 5),
+(5, 'Folding saw', 5),
+(6, 'Whistle', 5),
+(7, 'Sewing kit', 5),
+(8, 'Small mirror', 5),
+(9, 'GPS', 5),
+(10, 'First aid kit', 5),
+(11, 'Flashlight', 5),
+(12, 'Hand warmers/Toe warmers', 5);
 
 -- --------------------------------------------------------
 
