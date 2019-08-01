7cad83d0b5463fca66af69d971fca8da7368a5ff
Reese Lee <reese.lee.cy@gmail.com>
2019-08-01 00:57:38 -0700

    add FoodList component and code to list each food item

---
+++ ClientApp/src/components/FoodList.jsx
@@ -0,0 +1,34 @@
+import React, { Component } from 'react';
+import './Home.css';
+import { Layout } from './Layout';
+import Food from './Food';
+import PropTypes from 'prop-types';
+import { Link } from 'react-router-dom';
+
+function FoodList(props) {
+
+    return (
+      <Layout>
+      <div>
+        <h1 className="title4">Food</h1>
+          <div>
+            {props.foods.map(food => {
+              return <Food name={food.foodName}
+              id={food.foodId}
+              key={food.id} />
+            })}
+          </div>
+          <div className="buttonDiv">
+            <button className="listsButton"><Link to='/Lists'>Back to Lists</Link></button>
+          </div>
+      </div>
+      </Layout>
+    )
+
+}
+
+export default FoodList;
+
+FoodList.propTypes = {
+  foods: PropTypes.array
+}
